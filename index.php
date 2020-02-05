ebe630825a8e2f6382ba462f2ab4ee43885eae26
Bas van Zutphen <bas.vanzutphen@edu-kw1c.nl>
2020-02-05 12:15:07 +0100

    H2

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
-echo "hello wereld"
+/**
+*User:Bas van Zutphen
+*Date: 5-2-2020
+*Time: 11:55
+*File: index.php
+*/
 ?>
+<link href="h2/css/style.css" type="text/css" rel="stylesheet">
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="./h2/opdrachten/opdracht%202.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="h2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+
 
