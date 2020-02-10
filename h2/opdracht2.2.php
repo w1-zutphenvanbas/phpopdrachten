4a4f71b699370fa16527ef6d2a046ce8aff6e917
Bas van Zutphen <bas.vanzutphen@edu-kw1c.nl>
2020-02-10 14:01:32 +0100

    H2

--- h2/opdracht2.2.php
+++ h2/opdracht2.2.php
@@ -1,1 +1,88 @@
 <?php
+/**
+*User:Bas van Zutphen
+*Date: 10-2-2020
+*Time: 13:39
+*File: opdracht2.2.php
+*/
+?>
+<link href="../css/style.css" type="text/css" rel="stylesheet">
+<?php
+$txt1="Hallo";
+$txt2="een makkelijke taal";
+$txt14="toch zo'n makkelijke taal";
+$txt3="Wat is";
+$txt4="PHP";
+$txt5="Nooit gedacht dat";
+$txt6="De installatie is best ingewikkeld";
+$txt7="Fijn";
+$txt8="?";
+$txt9=".";
+$txt10=",";
+$txt11="<br>";
+$txt12="is";
+$txt13="vind je niet";
+?>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="./opdracht%202.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="./opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <p>
+        <?php echo $txt1;?>
+        <?php echo $txt10;?>
+        <?php echo $txt3;?>
+        <?php echo $txt4;?>
+        toch een makkelijke taal
+        <?php echo $txt9;?>
+        <?php echo $txt11;?>
+        <?php echo $txt6;?>
+        <?php echo $txt9;?>
+        <?php echo $txt7;?>
+        toch
+        <?php echo $txt8;?>
+        <?php echo $txt11;?>
+        <?php echo $txt5;?>
+        <?php echo $txt4;?>
+        <?php echo $txt14;?>
+        <?php echo $txt12;?>
+        <?php echo $txt9;?>
+    </p>
+    <p>
+        <?php echo $txt1;?>
+        <?php echo $txt10;?>
+        <?php echo $txt11;?>
+        <?php echo $txt7;?>
+        toch dat
+        <?php echo $txt4;?>
+        <?php echo $txt14;?>
+        <?php echo $txt12;?>
+        <?php echo $txt9;?>
+        <?php echo $txt11;?>
+        <?php echo $txt6;?>
+        <?php echo $txt9;?>
+        <?php echo $txt13;?>
+        <?php echo $txt8;?>
+    </p>
+   
+</main>
+</body>
+
+
