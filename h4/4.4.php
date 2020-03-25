2c41571147032dd3d0bda3654ed42a82a6c79fef
Bas van Zutphen <bas.vanzutphen@edu-kw1c.nl>
2020-03-25 11:32:38 +0100

    4.2 aangwpast

---
+++ h4/4.4.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ *User:Bas van Zutphen
+ *Date: 12-2-2020
+ *Time: 11:29
+ *File: 3.1.php
+ */
+?>
+<?php
+include "../Includes/header.php";
+include "script.php";
+?>
+<link href="../footer.css" rel="stylesheet" type="text/css">
+
+<body>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="../index.php">Terug</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    </p>
+    <p>
+        <?php
+        echo "<h1>oefening 4.4</h1>";
+        // Vandaag
+        $nu = strtotime("now");
+        $datumVandaag = date('d-m-y', $nu);
+
+        // Dag van de week 0-7
+        $datumDag = date('w', $nu);
+        echo "Vandaag is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo "<br>";
+        $dagenWeek = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");
+        echo "Vandaag is het ".$dagenWeek[$datumDag] . " en datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+1 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+2 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+3 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+4 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+5 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        // Morgen
+        $nu = strtotime('+6 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+        // Morgen
+        $nu = strtotime('+7 days');
+        $datumVandaag = date('d-m-y', $nu);
+        $datumDag = date('w',$nu);
+
+        echo "<br>Morgen is het de ".$datumDag. " dag en de datum is ".$datumVandaag;
+        echo"<br>";
+        echo "Morgen is het ".$dagenWeek[$datumDag]. " en de datum is ". $datumVandaag;
+
+        ?>
+
+        <?php
+        ?>
+
+    </p>
+</main>
+<?php include "../Includes/variabelen.php"?>
+<?php include "../Includes/Footer.php"?>
+
+
+
